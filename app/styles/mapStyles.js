9040f6498302ee04d9fa04b0cda0d4039a7a79e5
riveerxd <lukas.hrehor@gmail.com>
2024-04-26 23:39:33 +0200

    added file with custom map styles

---
+++ app/styles/mapStyles.js
@@ -0,0 +1,20 @@
+export const customMapStyle = [
+    {
+        featureType: 'poi',
+        elementType: 'labels',
+        stylers: [
+            {
+                visibility: 'off',
+            },
+        ],
+    },
+    {
+        featureType: 'transit.station',
+        elementType: 'labels',
+        stylers: [
+            {
+                visibility: 'off',
+            },
+        ],
+    },
+];
